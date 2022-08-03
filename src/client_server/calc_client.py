da3f7a6dd246aec623a8ef6935108174d4288b04
Zeref Yagami <100072216+sriram54@users.noreply.github.com>
2022-08-03 13:36:57 +0530

    added comments for readablity

--- src/client_server/calc_client.py
+++ src/client_server/calc_client.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-
+# importing required moduels
 import rospy
 import random
 from ros_essentials_cpp.srv import ros_calculator
 from ros_essentials_cpp.srv import ros_calculatorRequest
 
 if __name__ == "__main__":
+# creating node, connecting with server 
     rospy.init_node("calculator_client")
 
     calc_client = rospy.ServiceProxy("ros_calc", ros_calculator)
-
+    
+# giving random inputs from 1 to 10 to server
     r = rospy.Rate(1)
     while not rospy.is_shutdown():
         a = random.randint(1,10)
@@ -17,7 +19,9 @@ if __name__ == "__main__":
         c = random.randint(1,10)
 
         print(f"Generated [{a},{b},{c}], sending req to ros_calc_service")
-
+        
+        
+#assigning values for request flie        
         req = ros_calculatorRequest()
         req.a = a
         req.b = b
@@ -36,7 +40,9 @@ if __name__ == "__main__":
         print(f"Received calc response: {resp_mul.mul}")
         print(f"Received calc response: {resp_divide.divide}")
         print(f"Received calc response: {resp_square.square}")
-        print(f"Received calc response: {resp_square_root.squareroot}")
+        print(f"Received calc response: {resp_square_root.squareroot}\n\n")
 
 
         r.sleep()
+        
+''' This is a simple ros_calculator client which gives input to server for taking action on it and it prints the output returned from server'''
